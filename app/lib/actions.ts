4fee50100519a08b681a20c4faf94a5c31cd12fb
MOHD <iamabdullahzubair@gmail.com>
2024-10-12 23:03:31 +0530

    fixing linting errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -82,6 +82,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
+    console.error(error)
     return {
       message: "Database Error : Failed to create Invoice",
     };
@@ -121,6 +122,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
   } catch (error) {
+    console.error(error)
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -134,6 +136,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
   } catch (error) {
+    console.error(error)
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
